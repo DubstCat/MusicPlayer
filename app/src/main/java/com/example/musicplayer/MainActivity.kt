cceeb1490f11e2748eb5f65bf3cce35f0dff3d57
DubstCat <voronets.yar@gmail.com>
2021-10-20 23:36:27 +0300

    working player with some bugs

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.musicplayer
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
-import android.provider.MediaStore
+import android.os.Looper
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.musicplayer.data.Track
 import com.example.musicplayer.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.InputStream
+import java.lang.Exception
 import java.lang.StringBuilder
 import java.lang.reflect.Type
 import java.util.*
@@ -18,37 +21,86 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var mediaPlayer:MediaPlayer
-    var position = 0
-    val handler = Handler()
+    private var position = 0
+    lateinit var updater:Runnable
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val tracklist = loadTracks()
 
-        mediaPlayer = MediaPlayer()
-
-        binding.btnPlay.setOnClickListener{
-            if(mediaPlayer.isPlaying){
-                handler.removeCallbacks(updater)
-                mediaPlayer.pause()
-                binding.btnPlay.setImageResource(R.drawable.ic_baseline_pause_24)
-            }else{
+        binding.btnPlay.setOnClickListener {
+            if (!mediaPlayer.isPlaying) {
                 mediaPlayer.start()
+                binding.btnPlay.setImageResource(R.drawable.ic_baseline_pause_24)
+            } else {
+                mediaPlayer.pause()
                 binding.btnPlay.setImageResource(R.drawable.ic_baseline_play_arrow_24)
-                updateSeekBar()
             }
-            binding.btnNext.setOnClickListener{
-                mediaPlayer.setDataSource(tracklist[++position].trackUri)
-                mediaPlayer.prepare()
-                binding.tvTimer?.text = millisecondsToTimer(mediaPlayer.duration.toLong())
+        }
+
+        binding.btnNext.setOnClickListener {
+            handler.removeCallbacks(updater)
+            mediaPlayer.release()
+            prepareMediaPlayer(if(position==tracklist.size-1)tracklist[0].also { position = 0 }else tracklist[++position])
+            binding.btnPlay.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        }
+        binding.btnPrev.setOnClickListener{
+            handler.removeCallbacks(updater)
+            mediaPlayer.release()
+            prepareMediaPlayer(if(position==0)tracklist[tracklist.size-1].also { position = tracklist.size-1 }else tracklist[--position])
+            binding.btnPlay.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        }
+
+
+        binding.sbMain?.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, pos: Int, chanded: Boolean) {
+                if (chanded){
+                    mediaPlayer.seekTo(pos)
+                }
             }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                // pass
+            }
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                // pass
+            }
+        })
+        prepareMediaPlayer(tracklist[0])
+    }
+
+    private fun prepareMediaPlayer(track: Track){
+        try {
+            mediaPlayer = MediaPlayer()
+            mediaPlayer.setDataSource(track.trackUri)
+            mediaPlayer.prepareAsync()
+            binding.tvTimer?.text = millisecondsToTimer(mediaPlayer.duration.toLong())
+            binding.tvTitle?.text = track.title
+            binding.tvAuthor.text = track.artist
+            Glide.with(this)
+                .load(track.bitmapUri)
+                .centerCrop()
+                .into(binding.ivCover)
+            binding.sbMain?.progress = 0
+            binding.sbMain?.max = track.duration
+            mediaPlayer.setOnBufferingUpdateListener { _, progress ->
+                binding.sbMain?.secondaryProgress = progress
+            }
+            updater = Runnable {
+                binding.sbMain?.progress = mediaPlayer.currentPosition
+                binding.tvTimer?.text = millisecondsToTimer(mediaPlayer.currentPosition.toLong())
+                handler.postDelayed(updater, 1000)
+            }
+            handler.postDelayed(updater, 1000)
+
+        }catch (e:Exception){
+            e.printStackTrace()
         }
     }
 
-
-
-    fun loadTracks():List<Track>{
+    private fun loadTracks():List<Track>{
         val inputStream:InputStream = resources.openRawResource(R.raw.tracklist)
         val scanner = Scanner(inputStream)
         val builder = StringBuilder()
@@ -59,33 +111,18 @@ class MainActivity : AppCompatActivity() {
         return getTracklistFromJson(builder.toString())
     }
 
-    fun getTracklistFromJson(s:String):List<Track>{
+    private fun getTracklistFromJson(s:String):List<Track>{
         val gson = Gson()
         val listType:Type = object :TypeToken<List<Track>>(){}.type
         return gson.fromJson(s, listType)
     }
 
-    private val updater = Runnable {
-        kotlin.run {
-            updateSeekBar()
-            val currendDuration = mediaPlayer.currentPosition
-            binding.tvTimer?.text = millisecondsToTimer(currendDuration.toLong())
-        }
-    }
-
-    private fun updateSeekBar(){
-        if(mediaPlayer.isPlaying){
-            binding.seekBar.progress = (((mediaPlayer.currentPosition).toFloat()) / (mediaPlayer.duration).toFloat()).toInt()
-            handler.postDelayed(updater, 1000)
-        }
-    }
-
     fun millisecondsToTimer(milliseconds: Long):String{
         var timerString = ""
         var secondString = ""
-        var hours = (milliseconds/(1000*60*60)).toInt()
-        var minutes = ((milliseconds%(1000*60*60))/(1000*60)).toInt()
-        var seconds = ((milliseconds % (1000 * 60 * 60)) % (1000*60)/1000).toInt()
+        val hours = (milliseconds/(1000*60*60)).toInt()
+        val minutes = ((milliseconds%(1000*60*60))/(1000*60)).toInt()
+        val seconds = ((milliseconds % (1000 * 60 * 60)) % (1000*60)/1000).toInt()
 
         if(hours>0){
             timerString = "$hours:"
@@ -95,10 +132,7 @@ class MainActivity : AppCompatActivity() {
         }else{
             secondString = "$seconds"
         }
-
-        timerString = timerString + minutes + ":" + secondString
+        timerString = "$timerString$minutes:$secondString"
         return timerString
-
     }
-
 }
